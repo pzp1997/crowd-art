4632ab267251a790916bd1698dac251d733d572b
Palmer Paul <pzpaul2002@yahoo.com>
2019-04-18 15:36:15 -0400

    add timestamps to heatmap

--- src/heatmap/main.js
+++ src/heatmap/main.js
@@ -5,9 +5,9 @@ var ctx = canvas.getContext("2d");
 
 var clicks = [];
 
-function drawDot(x, y, color) {
+function drawDot(x, y) {
   ctx.beginPath();
-  ctx.fillStyle = color;
+  ctx.fillStyle = "red";
   ctx.arc(x, y, dotDiameter, 0, 2 * Math.PI);
   ctx.fill();
 }
@@ -16,12 +16,13 @@ function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
+    y: evt.clientY - rect.top,
+    timestamp: Date.now(),
   };
 }
 
 canvas.addEventListener("click", function(evt) {
   var mousePos = getMousePos(canvas, evt);
-  drawDot(mousePos.x, mousePos.y, "red");
+  drawDot(mousePos.x, mousePos.y);
   clicks.push(mousePos);
 });
