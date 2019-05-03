01f708d1114fb78e858e016198a71cdb0fcafea3
Palmer Paul <pzpaul2002@yahoo.com>
2019-05-03 17:19:06 -0400

    update mosaic animate script

--- src/animate/mosaic_animate.py
+++ src/animate/mosaic_animate.py
@@ -5,57 +5,68 @@ import imageio
 from collections import defaultdict
 import os
 
-mosaic_df = pd.read_csv('mosaic_peers.csv')
 
-segments = defaultdict(list)
+def points_from_batch(batch_csv):
+    mosaic_df = pd.read_csv(batch_csv)
+    segments = defaultdict(list)
 
-# each segment has list of responses
-# each response has list of lines (might be undone)
-# each line has data field with list of points
+    # each segment has list of responses
+    # each response has list of lines (might be undone)
+    # each line has data field with list of points
 
-THICK_LINES = True
+    undo_usage = 0
 
-undo_usage = 0
-
-for _, row in mosaic_df.iterrows():
-    segment = row['Input.image']
-    response = row['Answer.response']
-    response = json.loads(response)
-    points = []
-
-    for line in response:
-        if line['undoTimestamp'] is not None:
-            undo_usage += 1
-            continue
-
-        for point in line['data']:
-            points.append((round(point['x']), round(point['y']), point['timestamp']))
-
-    segments[segment].append(points)
-
-def create_image_from_points(img_filename, points, width, height):
-    if any(x in img_filename for x in ['0_2', '0_3', '2_0']):
+    for _, row in mosaic_df.iterrows():
+        segment = row['Input.image']
+        response = row['Answer.response']
+        response = json.loads(response)
         points = []
-    if len(points) > 0:
-        xs, ys, _ = zip(*points)
-        print(img_filename)
-        print('min width:', min(xs), 'max width:', max(xs), 'min height:', min(ys), 'max height:', max(ys))
-    img_data = np.zeros((height, width))
+
+        for line in response:
+            if line['undoTimestamp'] is not None:
+                undo_usage += 1
+                continue
+
+            for point in line['data']:
+                points.append((round(point['x']), round(
+                    point['y']), point['timestamp']))
+
+        segments[segment].append(points)
+
+    return segments
+
+
+def frames_from_points(points, width, height):
+    # if len(points) > 0:
+    #     xs, ys, _ = zip(*points)
+    #     print(img_filename)
+    #     print('min width:', min(xs), 'max width:', max(xs), 'min height:', min(ys), 'max height:', max(ys))
+
+    img_data = np.zeros((height, width), dtype=np.uint8)
     for x, y, _ in points:
-        img_data[y, x] = 1
-        if THICK_LINES:
-            img_data[y + 1, x] = 1
-            img_data[y, x + 1] = 1
-            img_data[y + 1, x + 1] = 1
-    imageio.imwrite(img_filename, img_data)
+        img_data[y:y + 2, x:x + 2] = 0xff  # white
+    return img_data
 
-for segment, responses in segments.items():
-    points = [point for response in responses for point in response]
-    points.sorted(key=lambda x: x[2])
 
-    create_image_from_points(
-        os.path.join('mosaic_segments', segment),
-        points,
-        604,
-        481,
-    )
+def main():
+    segments = points_from_batch('mosaic_peers.csv')
+
+    for segment, responses in segments.items():
+        if any(x in segment for x in ['0_2', '0_3', '2_0']):
+            points = []
+        else:
+            points = [point for response in responses for point in response]
+            points.sort(key=lambda x: x[2])
+
+        frame = frames_from_points(
+            points,
+            604,
+            481,
+        )
+
+        imageio.imwrite(os.path.join('mosaic_segments', segment), frame)
+
+    print('successfully wrote images to file')
+
+if __name__ == '__main__':
+    main()
