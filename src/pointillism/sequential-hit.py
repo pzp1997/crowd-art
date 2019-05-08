2914d7a0f5d5dbbbae3f1e39d5aadedf45ec87da
Palmer Paul <pzpaul2002@yahoo.com>
2019-05-08 03:35:19 -0400

    update sequential-hit.py code

--- src/pointillism/sequential-hit.py
+++ src/pointillism/sequential-hit.py
@@ -2,29 +2,55 @@ import boto3
 import datetime
 import json
 
-HIT_TYPE_ID = '3KTTL66PWHEWV8ZFDTHWRAEV3ELVHE'
-LAYOUT_ID = '3WMOO8M43NBXEU3DVKBV8LDXYHD4LO'
+HIT_TYPE_ID = '37D300AOKNWAXMADSQZNL0933KXIDW'
+LAYOUT_ID = '3D5J2DF5KQGO2LON470T6AXXPJT9PO'
 SNS_TOPIC = 'arn:aws:sns:us-east-1:277402979440:crowd-art-pointillism-hit'
 SANDBOX = True
+EVENT_TYPES = ['AssignmentSubmitted']
 
 endpoint_url = 'https://mturk-requester{}.us-east-1.amazonaws.com'.format(
     '-sandbox' if SANDBOX else '')
 
 def lambda_handler(event, context):
-    msg = event['Records'][0]['Sns']['Message']
-    try:
-        msg = msg['Events']
-    except TypeError:
-        print('Recovering from TypeError')
-        msg = json.loads(msg)
-        msg = msg['Events']
-    msg = msg[0]
-    msg = msg['Answer']
-    msg_start = msg.find('<FreeText>') + len('<FreeText>')
-    msg_end = msg.find('</FreeText>')
-    msg = msg[msg_start:msg_end]
-    print('MESSAGE:', msg)
+    print('EVENT:', event)
 
+    records = event['Records']
+    first_record = records[0]
+    sns = first_record['Sns']
+    msg = sns['Message']
+    msg = json.loads(msg)
+    events = msg['Events']
+    first_event = events[0]
+
+    type_id = first_event['HITTypeId']
+    if type_id != HIT_TYPE_ID:
+        print('WRONG HIT TYPE ID:', type_id)
+        return
+
+    event_type = first_event['EventType']
+    if event_type not in EVENT_TYPES:
+        print('WRONG EVENT TYPE:', event_type)
+        return
+
+    parent_hit_id = first_event['HITId']
+    print('PARENT HIT ID:', parent_hit_id)
+
+    parent_assignment_id = first_event['AssignmentId']
+    print('PARENT ASSIGNMENT ID:', parent_assignment_id)
+
+    parent_timestamp = first_event['EventTimestamp']
+    print('PARENT TIMESTAMP:', parent_timestamp)
+
+    answer = first_event['Answer']
+    data_start = answer.find('<FreeText>') + len('<FreeText>')
+    data_end = answer.find('</FreeText>')
+    data = answer[data_start:data_end]
+    print('DATA:', data)
+
+    publish_hit(data)
+
+
+def publish_hit(data):
     client = boto3.client(
         'mturk',
         endpoint_url=endpoint_url,
@@ -32,28 +58,21 @@ def lambda_handler(event, context):
 
     resp = client.create_hit_with_hit_type(
         HITTypeId = HIT_TYPE_ID,
-        MaxAssignments = 1000,
+        MaxAssignments = 1,
         LifetimeInSeconds = int(datetime.timedelta(days=7).total_seconds()),
         HITLayoutId = LAYOUT_ID,
-        HITLayoutParameters = [{'Name': 'data', 'Value': msg}],
+        HITLayoutParameters = [{'Name': 'data', 'Value': data}],
     )
+    print('CREATE HIT RESPONSE:', resp)
 
     notification = {
         'Destination': SNS_TOPIC,
         'Transport': 'SNS',
         'Version': '2014-08-15',
-        'EventTypes': ['AssignmentSubmitted']
+        'EventTypes': EVENT_TYPES,
     }
 
     client.update_notification_settings(
         HITTypeId = HIT_TYPE_ID,
         Notification = notification,
-    )
-
-    output = {
-        'HIT_ID': resp['HIT']['HITId'],
-        'HIT_GROUP_ID': resp['HIT']['HITGroupId'],
-        'INPUT': msg,
-    }
-    print('OUTPUT:', output)
-    return output
+    ) 
